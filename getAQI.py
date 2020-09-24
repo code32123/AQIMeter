f4a4c75d8da7da7201db3a13b4c6285890bc15f1
Jimmy S <jimmy.l.smythe@gmail.com>
2020-09-24 09:54:10 -0700

    Fix "Biased" Logs in getAQI

--- getAQI.py
+++ getAQI.py
@@ -3,7 +3,7 @@
 #	Commenting
 #	Logging
 
-#This is a program designed to webscrape the airnow.gov website, and find the current AQI data in eugene OR
+# This is a program designed to webscrape the airnow.gov website, and find the current AQI data in eugene OR
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,60 +25,60 @@ def resource_path(relative_path):
 
 def getAQI(url = "https://www.airnow.gov/?city=Eugene&state=OR&country=USA"):
 
-	LoggerPrint("Getting AQI for Eugene OR", "Always")
+	LoggerPrint("Getting AQI", "Always")
 
-	#Stuff to get chrome to work
+	# Stuff to get chrome to work
 	options = Options()  
 	options.add_argument("--headless") 
 
 	options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-	#Starting chrome
+	# Starting chrome
 	LoggerPrint("Initializing Chrome", "Information")
 	LoggerPrint(str(resource_path('Chrome\\chromedriver.exe')),"Debug")
 	browser = webdriver.Chrome(options=options, executable_path=resource_path('Chrome\\chromedriver.exe'))  
 	LoggerPrint("Chrome Browser Initialized in Headless Mode", "Debug")
 
-	#Get the url
+	# Get the url
 	LoggerPrint("Getting url:  " + url, "Information")
 	browser.get(url)
 
-	#The delay in seconds
+	# The delay in seconds
 	delay = 3
 
 	try:
-		#Waits for three seconds. Don't ask me why, it just works.
+		# Waits for three seconds. Don't ask me why, it just works.
 		LoggerPrint("Waiting for webpage to load", "Information")
 		myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'SHOULDNOTEXSIST')))
 		LoggerPrint(" 'SHOULDNOTEXSIST', exists! The page may not be fully loaded!", "Warning")
 	except TimeoutException:
-		#Expected. IS NOT BAD. Just means it waited enough for the page to load.
+		# Expected. IS NOT BAD. Just means it waited enough for the page to load.
 		LoggerPrint("Loaded!", "Debug")
 
-	#Save the inner html to a variable
+	# Save the inner html to a variable
 	html = browser.page_source
 
-	#Quitting chrome
+	# Quitting chrome
 	browser.quit()
 	LoggerPrint("Browser Exited", "Information")
 
-	#Takes the innerhtml, and saves it as text.
+	# Takes the innerhtml, and saves it as text.
 	html = html.encode('cp850','replace').decode('cp850')
 
-	#Finds AQI
+	# Finds AQI
 	i = html.find('<div class="aqi" data-tippy=""><b>')
 	aqi = html[i+34:i+37]
 
-	#Trims, if necessary
+	# Trims, if necessary
 	if "<" in aqi:
 		aqi = aqi[0:2]
 
-	#Prints and returns
+	# Prints and returns
 	LoggerPrint(aqi, "Debug")
 
 	now = datetime.now()
 	dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 	prepend_line("AQILog.txt", dt_string + "-->  " + aqi, folder="./Logs/")
 
-	LoggerPrint("Got AQI for Eugene OR", "Always")
+	LoggerPrint("Got AQI", "Always")
 	return int(aqi)
